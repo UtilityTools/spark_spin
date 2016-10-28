a69a70fc110580af53b5a140f4f3e33326c1ec1a
Rene Richard <rene.richard@nrc.ca>
2016-10-28 10:40:45 -0300

    Added schema and Person domain object to streaming job

--- apps/stream_avg/src/main/java/ca/redsofa/jobs/StructuredStreamingAverage.java
+++ apps/stream_avg/src/main/java/ca/redsofa/jobs/StructuredStreamingAverage.java
@@ -1,67 +1,44 @@
-/*
-TODO :
--------
-Person Object that supports
-- Male, count
-- Female, count 
-
-- File stream instead of soket
-- Average age Calculation
-- Persist State
-- Output to JSON
-
-//Taken from Apache Spark Example sources... Will be modified with requirements above :
-
-In current state , 
-- run "nc -lk 9999" in one terminal
-- run sh submit_job.sh in the other
-
-- Type a phrase in the first terminal and hit return key
-*/
-
-
 package ca.redsofa.jobs;
 
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.streaming.StreamingQuery;
-
-import java.util.Arrays;
-import java.util.Iterator;
+import ca.redsofa.domain.Person;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.types.StructType;
 
 
 public final class StructuredStreamingAverage {
-  public static void main(String[] args) throws Exception {
+  private static String INPUT_DIRECTORY = "/home/richardr/streaming_test/input_files";
 
+  public static void main(String[] args) throws Exception {
+    System.out.println("Starting StructuredStreamingAverage job...");
     SparkSession spark = SparkSession
       .builder()
       .appName("StructuredStreamingAverage")
       .getOrCreate();
 
-    // Create DataFrame representing the stream of input lines from connection to host:port
-    Dataset<String> lines = spark
+
+    StructType personSchema = new StructType()
+                                    .add("firstName", "string")
+                                    .add("lastName", "string")
+                                    .add("sex", "string")
+                                    .add("age", "long");
+
+    //Create DataFrame representing the stream of input files
+    Dataset<Person> personStream = spark
       .readStream()
-      .format("socket")
-      .option("host", "localhost")
-      .option("port", 9999)
-      .load().as(Encoders.STRING());
+      .schema(personSchema)
+      .json(INPUT_DIRECTORY)
+      .as(Encoders.bean(Person.class));
+       
+      personStream.createOrReplaceTempView("people");
+      Dataset<Row> ageAverage = spark.sql("SELECT AVG(age) as average_age, sex FROM people GROUP BY sex");
 
-    // Split the lines into words
-    Dataset<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-      @Override
-      public Iterator<String> call(String x) {
-        return Arrays.asList(x.split(" ")).iterator();
-      }
-    }, Encoders.STRING());
-
-    // Generate running word count
-    Dataset<Row> wordCounts = words.groupBy("value").count();
-
-    // Start running the query that prints the running counts to the console
-    StreamingQuery query = wordCounts.writeStream()
-      .outputMode("complete")
-      .format("console")
-      .start();
+      StreamingQuery query = ageAverage.writeStream()
+        .outputMode("complete")
+        .format("console")
+        .start();
 
     query.awaitTermination();
   }
